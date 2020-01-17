c7162080514e9761cae30b86c05b9812bfd2c823
eliasdosreis <elias_dos_reis@hotmail.com>
2020-01-17 17:28:03 -0300

    Handlebars_1

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 
 const handlebars = require('express-handlebars')
-
 // Config 
     // Template Engine
     app.engine('handlebars',handlebars ({ defaultLayout : 'main'}))
